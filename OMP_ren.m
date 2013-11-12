60021b88db98b6fd50af8323f52dbdb33f7d03de
freefood89 <freefood89@gmail.com>
2013-11-12 18:06:01 -0500

    OMP_ren works, image recovery performed successfulyy without regularization

--- OMP_ren.m
+++ OMP_ren.m
@@ -1,7 +1,7 @@
-function alpha = OMP_ren(block,C,lambda)
+function alpha = OMP_ren(B,C,K,lambda)
 
-normC=C
-norms = zeros(1,blocklen^2);
+normC=C;
+norms = zeros(1,K^2);
 means = mean(normC);
 for n=1:size(normC,2)
     norms(n) = norm(normC(:,n));
@@ -10,15 +10,15 @@ for n=1:size(normC,2)
 end
 
 prod=0;
-alpha=zeros(blocklen^2,1);
+alpha=zeros(K^2,1);
 F = B;
 omega = [];
-a = zeros(1,blocklen^2); % Convenient for debug
+a = zeros(1,K^2); % Convenient for debug
 for p=1:lambda
     % Find Ai with largest inner product
     index = 0;
     prod = 0;
-    for n=1:blocklen^2 % optimize later (dont iterate if it's been picked)
+    for n=1:K^2 % optimize later (dont iterate if it's been picked)
         a(n) = abs(F'*normC(:,n));
         if(a(n)>prod && (sum(omega==n)==0))
             prod=a(n);
--- ece660_proj2.m
+++ ece660_proj2.m
@@ -5,50 +5,11 @@ K = 8;
 A = dctmtx_ren(K);
 image = imgRead('fishing_boat.bmp');
 blocks = imblock_ren(image,K);
+
 %%
-lambda=[30];
-
 for blk=1:size(blocks,2)
-    ind=sort(randperm(K^2,lambda(end)));
-    B=blocks(ind,blk);
-    C = A(ind,:);
-    normC=A(ind,:);
-    norms = zeros(1,K^2);
-    means = mean(normC);
-    for n=1:size(normC,2)
-        norms(n) = norm(normC(:,n));
-        normC(:,n) = normC(:,n)-means(n);
-        normC(:,n) = normC(:,n)/norm(normC(:,n));
-    end
-
-    prod=0;
-    alpha=zeros(K^2,1);
-    F = B;
-    omega = [];
-    a = zeros(1,K^2); % Convenient for debug
-    for p=1:lambda
-        % Find Ai with largest inner product
-        index = 0;
-        prod = 0;
-        for n=1:K^2 % optimize later (dont iterate if it's been picked)
-            a(n) = abs(F'*normC(:,n));
-            if(a(n)>prod && (sum(omega==n)==0))
-                prod=a(n);
-                index=n;
-            end
-        end
-        %     plot(a), title(['A_i, i=' num2str(p)]);
-        %     display(index);
-        omega = [omega index];
-        
-        % solve for alpha using residuals
-        for n=1:length(omega)
-            alpha(omega) = C(:,omega)\B;
-        end
-        % Calculating residue with Ai & ai
-        F = B - C*alpha;
-        %     norm(F)
-    end
+    ind=sort(randperm(K^2,50));
+    alpha = OMP_ren(blocks(ind,blk),A(ind,:),K,40);
     cSensed(:,blk) = A*alpha;
     blk
 end
@@ -60,24 +21,3 @@ subplot(132), imgShow(medfilt2(imassemble_ren(cSensed,size(image,1),size(image,2
 title('Median Filtered');
 subplot(133), imgShow(image);
 title('Original');
-% %%
-% figure;
-% subplot(131); imgShow((reshape(A*alpha,8,8))); title('OMP');
-% subplot(132); imgShow(medfilt2(reshape(A*alpha,8,8),[3 3])); title('OMP');
-% subplot(133); imgShow(reshape(blocks(:,1),8,8)); title('original');
-
-% figure;
-% imgShow(imassemble_ren(blocks,25,24));
-
-lambda = 10:10:50;
-Error = zero(1,length(lambda));
-setSize = size
-for L=1:length(lambda)
-    for set=1:4
-        
-        orig
-        cSensed
-        Error(set) = imgCompare(orig,cSensed);
-
-    end
-end
