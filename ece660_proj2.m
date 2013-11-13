1e13aada34dc7bc218c4fea3acdaf1cfefb99581
freefood89 <freefood89@gmail.com>
2013-11-13 17:33:59 -0500

    fixed some errors in saving figures

--- ece660_proj2.m
+++ ece660_proj2.m
@@ -7,20 +7,23 @@ blocks = imblock_ren(image,K);
 S = [10 20 30 40 50];
 e_recover = zeros(1,length(S));
 e_filt = zeros(1,length(S));
-for n=1:length(S)
+for n=1:1%length(S)
    imgOut = imgRecover(image,K,S(n)); 
    filtered = medfilt2(imgOut,[3 3]);
    imwrite(uint8(imgOut),['boat' num2str(S(n)) '.png'],'png'); 
    imwrite(uint8(filtered),['boat_filt' num2str(S(n)) '.png'],'png');
    e_recover(n) = sum(sum((imgOut-image).^2))/size(image,1)/size(image,2);
    e_filt(n) = sum(sum((filtered-image).^2))/size(image,1)/size(image,2);
-   figure(1,'visible','off'); imgShow(imgOut); title('Compressed Sensing');
-   saveas(1,['boat' num2str(S(n))],'fig')
-   figure(1,'visible','off'); imgShow(filtered); title('Median Filtered');
-   saveas(1,['boat_filt' num2str(S(n))],'fig')
+   h=figure; imgShow(imgOut); title('Recovered');
+   saveas(h,['boat' num2str(S(n))],'fig')
+   h=figure; imgShow(filtered); title('Median Filtered');
+   saveas(h,['boat_filt' num2str(S(n))],'fig')
 end
-save('boat_errors.mat',e_recover,e_filt);
+
+save('boat_errors.mat','e_recover','e_filt');
 %%
+close all;
+
 K = 16;
 image = imgRead('lena.bmp');
 blocks = imblock_ren(image,K);
@@ -40,14 +43,3 @@ for n=1:length(S)
    saveas(1,['lena_filt' num2str(S(n))],'fig');
 end
 save('boat_errors.mat',e_recover,e_filt);
-% %%
-% imgOut = imgRecover(image,8,30);
-% 
-% figure; imgShow(imgOut);
-% title('Compressed Sensing');
-% filtered = medfilt2(imgOut,[3 3]);
-% figure, imgShow(filtered);
-% title('Median Filtered');
-% 
-% e_recover = sum(sum((imgOut-image).^2))/size(image,1)/size(image,2)
-% e_filt = sum(sum((filtered-image).^2))/size(image,1)/size(image,2)
